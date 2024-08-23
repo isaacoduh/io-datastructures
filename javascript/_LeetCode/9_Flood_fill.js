e30d62d2a83e286fd5194ace21c91519d5812c0e
Isaac Oduh <ufuomaoduh@gmail.com>
2024-08-23 22:29:46 +0100

    js:leetcode - flood fill

---
+++ javascript/_LeetCode/9_Flood_fill.js
@@ -0,0 +1,60 @@
+/**
+ * Description
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+
+Return the modified image after performing the flood fill.
+ * 
+ * 
+ * Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+    Output: [[2,2,2],[2,2,0],[2,0,1]]
+    Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+    Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+ * 
+ * 
+ */
+
+var floodFill = function (image, sr, sc, color) {
+  const m = image.length;
+  const n = image[0].length;
+  const target = image[sr][sc];
+
+  const dfs = (i, j) => {
+    if (
+      i < 0 ||
+      i === m ||
+      j < 0 ||
+      j === n ||
+      image[i][j] !== target ||
+      image[i][j] === color
+    ) {
+      return;
+    }
+    image[i][j] = color;
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  };
+  dfs(sr, sc);
+  return image;
+};
+
+// Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+// Output: [[2,2,2],[2,2,0],[2,0,1]]
+
+console.log(
+  floodFill(
+    [
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 0, 1],
+    ],
+    1,
+    1,
+    2
+  )
+);
