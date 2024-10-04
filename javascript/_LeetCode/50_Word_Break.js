0e7ac1fa0d833e50e77a4a7f04508c4000d26aef
Isaac Oduh <ufuomaoduh@gmail.com>
2024-10-04 21:23:01 +0100

    js:leetcode - word break

---
+++ javascript/_LeetCode/50_Word_Break.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    let dp = new Array(s.length + 1).fill(false);
+    dp[0] = true;
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let w of wordDict) {
+            let start = i - w.length;
+            if (start >= 0 && dp[start] && s.substring(start, i) === w) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[s.length];    
+};
+
+
+https://leetcode.com/problems/word-break/solutions/5544493/video-using-dynamic-programming
